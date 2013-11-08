384acbe995f768622bfe3c7ea6d76f4c227c33a9
gcq <gcquiroga@gmail.com>
2013-11-08 23:31:56 +0100

    Basic functionality
    Added the CloudConvert class. A mirror to the online service. Currently
    i'm getting Exceptions while validating the SSL certificate.

---
+++ CloudConvert.py
@@ -0,0 +1,66 @@
+import requests
+
+
+class CloudConvert():
+    def __init__(self, apikey):
+        self.apikey = apikey
+
+    def _start(self, inputformat, outputformat, apikey=None):
+        if apikey is None:
+            apikey = self.apikey
+
+        url = (
+            "https://api.cloudconvert.org/process?"
+            "inputformat={inputf}&outputformat={outputf}&apikey={api}"
+            ).format(
+            inputf=inputformat,
+            outputf=outputformat,
+            api=self.apikey)
+
+        return requests.get(url).text
+
+    def _upload(self, pid, fname, outformat, options=None):
+        url = (
+            "https://srv01.cloudconvert.org/process/{pid}"
+            ).format(pid=pid)
+
+        if options is None:
+            options = {}
+
+        with open(fname, "r") as f:
+            requests.post(url,
+                          data={
+                              "outputformat": outformat
+                              },
+                          files={"file": f})
+
+    def _status(self, pid):
+        url = (
+            "https://srv01.cloudconvert.org/process/{pid}"
+            ).format(pid=pid)
+
+        return requests.get(url).json
+
+    def _cancel(self, pid):
+        url = (
+            "https://srv01.cloudconvert.org/process/{pid}/cancel"
+            ).format(pid=pid)
+
+        requests.get(url)
+
+    def _delete(self, pid):
+        url = (
+            "https://srv01.cloudconvert.org/process/{pid}/delete"
+            ).format(pid=pid)
+
+        requests.get(url)
+
+    def _list(self, apikey=None):
+        if apikey is None:
+            apikey = self.apikey
+
+        url = (
+            "https://api.cloudconvert.org/processes?apikey={api}"
+            ).format(api=apikey)
+
+        return requests.get(url).json
