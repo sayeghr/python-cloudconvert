b6919dd84260b410d048f66278d219f255664323
gcq <gcquiroga@gmail.com>
2013-11-09 13:52:45 +0100

    Added _download method to CloudConvert
    And made pid an optional argument

--- CloudConvert.py
+++ CloudConvert.py
@@ -4,6 +4,7 @@ import requests
 class CloudConvert():
     def __init__(self, apikey):
         self.apikey = apikey
+        self.pid = None
 
     def _start(self, inputformat, outputformat, apikey=None):
         if apikey is None:
@@ -19,13 +20,16 @@ class CloudConvert():
 
         return requests.get(url).text
 
-    def _upload(self, pid, fname, outformat, options=None):
+    def _upload(self, fname, outformat, pid=None, options=None):
+        if pid is None:
+            pid = self.pid
+            
         url = (
             "https://srv01.cloudconvert.org/process/{pid}"
             ).format(pid=pid)
 
         if options is None:
-            options = {}
+            options = {}  # TODO
 
         with open(fname, "r") as f:
             requests.post(url,
@@ -34,21 +38,38 @@ class CloudConvert():
                               },
                           files={"file": f})
 
-    def _status(self, pid):
+    def _status(self, pid=None):
+        if pid is None:
+            pid = self.pid
+            
         url = (
             "https://srv01.cloudconvert.org/process/{pid}"
             ).format(pid=pid)
 
         return requests.get(url).json()
 
-    def _cancel(self, pid):
+    def _download(self, pid=None):
+        if pid is None:
+            pid = self.pid
+
+        url = self._status(pid)["output"]["url"]
+
+        # TODO
+
+    def _cancel(self, pid=None):
+        if pid is None:
+            pid = self.pid
+            
         url = (
             "https://srv01.cloudconvert.org/process/{pid}/cancel"
             ).format(pid=pid)
 
         requests.get(url)
 
-    def _delete(self, pid):
+    def _delete(self, pid=None):
+        if pid is None:
+            pid = self.pid
+            
         url = (
             "https://srv01.cloudconvert.org/process/{pid}/delete"
             ).format(pid=pid)
